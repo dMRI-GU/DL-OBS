590a5192e7c0c49f08115d9db1ee73bdf46afbcd
CaviarLover <wenyin.zhou.halawin@gmail.com>
2022-07-27 10:13:30 +0200

    Multi-Decoders

---
+++ model/unet_MultiDecoder.py
@@ -0,0 +1,57 @@
+from model.unet_parts import *
+from model.utils import *
+
+class UNet_MultiDecoders(nn.Module):
+    def __init__(self, n_channels, b, rice=True,  bilinear=False):
+        super(UNet_MultiDecoders, self).__init__()
+        self.n_channels = n_channels
+        self.bilinear = bilinear
+        self.b_values = b.reshape(1, len(b), 1, 1)
+        self.rice = rice
+
+        self.inc = DoubleConv(n_channels, 64)
+        self.down1 = Down(64, 128)
+        self.down2 = Down(128, 256)
+        self.down3 = Down(256, 512)
+        factor = 2 if bilinear else 1
+        self.down4 = Down(512, 1024 // factor)
+        
+        channels = [1024, 512, 256, 128, 64]
+        self.decoder1 = Decoder(channels, factor, self.bilinear)
+        self.decoder2 = Decoder(channels, factor, self.bilinear)
+        self.decoder3 = Decoder(channels, factor, self.bilinear)
+        self.decoder4 = Decoder(channels, factor, self.bilinear)
+        
+
+    def forward(self, x):
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+
+        f_maps = [x4, x3, x2, x1]
+
+        d_1 = self.decoder1(f_maps, x5)
+        d_2 = self.decoder2(f_maps, x5)
+        f = self.decoder3(f_maps, x5)
+        sigma_g = self.decoder4(f_maps, x5)
+
+        if torch.mean(d_1) < torch.mean(d_2):
+            d_1, d_2 = d_2, d_1
+            f = 1 - f
+
+        d_1 = sigmoid_cons(d_1, 2, 2.4)
+        d_2 = sigmoid_cons(d_2, 0.1, 0.5)
+        f = sigmoid_cons(f, 0.5, 1.0)
+
+        v = bio_exp(d_1, d_2, f, self.b_values)
+
+        # add the rice-bias
+        if self.rice:
+            res = rice_exp(v, sigma_g)
+        else:
+            res = v
+
+        return res, d_1, d_2, f, sigma_g
+
---
+++ model/utils.py
@@ -0,0 +1,26 @@
+import torch
+from cmath import sqrt
+
+def sigmoid_cons(param, dmin, dmax):
+    """
+    constrain the output physilogical parameters into a certain domain
+    """
+    return dmin+torch.sigmoid(param)*(dmax-dmin)
+
+def rice_exp(v, sigma):
+    """
+    Add the rician bias
+    """
+    t = v / sigma
+    res= sigma*(sqrt(torch.pi/8)*
+                    ((2+t**2)*torch.special.i0e(t**2/4)+
+                    t**2*torch.special.i1e(t**2/4)))
+    res = res.to(torch.float32)
+    return res
+
+def bio_exp(d1, d2, f, b):
+    """ivim model"""
+    
+    v = f*torch.exp(-b*d1*1e-3) + (1-f)*torch.exp(-b*d2*1e-3)
+
+    return v
